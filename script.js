26543082c904b139cf06520c6529a0f393897e25
Alejandro Romero <37181533+VoidlessSeven7@users.noreply.github.com>
2019-06-12 10:56:33 +0000

    Added support for section "Recently updated"

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 // @name         Steam owned games remover
 // @homepage     https://github.com/VoidlessSeven7/steam-in-library-remover/
 // @namespace    http://tampermonkey.net/
-// @version      1.01
+// @version      1.05
 // @description  Removes from the store the games that you already own
 // @icon         http://store.steampowered.com/favicon.ico
 // @author       Alejandro Akbal
-// @match        https://store.steampowered.com/*
+// @include      https://store.steampowered.com/*
+// @include      http://store.steampowered.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -16,33 +17,82 @@
     // User Settings
     const settings = {
         removeOwned: true,
-        removeIgnored: false
+        removeIgnored: false,
+        removeOwnedRecentlyUpdatedList: false,
+        removeIgnoredRecentlyUpdatedList: false
     }
 
-    // Functionality
-
+    // Functionality condensed into a single function
     async function start() {
 
         // Function that removes elements by classes
         const removeElements = (elms) => elms.forEach(el => el.remove());
 
-        if (settings.removeOwned === true) {
-            removeElements( document.querySelectorAll(".ds_owned") );
-            console.log('Removed owned content');
-        }
+        /* Variable for checking if the current url is the updated game section
+            "< 0" means that it ISN'T the section
+            "> -1" means it is the section */
+        const urlContainsUpdated = window.location.href.indexOf("/updated/");
 
-        if (settings.removeIgnored === true) {
-            removeElements( document.querySelectorAll(".ds_ignored") );
-            console.log('Removed ignored content');
-        }
 
-    };
+        /* ----- Category: Normal usage ----- */
+
+        if(urlContainsUpdated < 0) {
+
+            if (settings.removeOwned === true) {
+                removeElements( document.querySelectorAll(".ds_owned") );
+                console.log('Removed owned content');
+            }
+
+            if (settings.removeIgnored === true) {
+                removeElements( document.querySelectorAll(".ds_ignored") );
+                console.log('Removed ignored content');
+            }
+
+        } // End of Category: Recently updated
+
+
+        /* ----- Category: Recently updated ----- */
+
+        if (urlContainsUpdated > -1) {
+
+                if (settings.removeOwnedRecentlyUpdatedList === true) {
+                    removeElements( document.querySelectorAll(".ds_owned") );
+                    console.log('Removed recently updated owned content');
+                }
+
+                if (settings.removeIgnoredRecentlyUpdatedList === true) {
+                    removeElements( document.querySelectorAll(".ds_ignored") );
+                    console.log('Removed recently updated ignored content');
+                }
+
+
+        } // End of Category: Recently updated
+
+
+    }; // End of function start
 
 
     // Executing the script
-    setTimeout(start, 50);
+    setTimeout(start, 1000);
 
     setInterval(start, 5000);
 
 
-})();
+
+
+
+
+
+
+
+/* ----- Code that im not gonna use but could be good for optimizing in the future -----
+
+    let isThereOwnedContent;
+    isThereOwnedContent = document.querySelectorAll(".ds_owned");
+    if (isThereOwnedContent.length > 0) {
+        alert("funciona");
+    }
+
+*/
+
+})(); // End of Tampermonkey script

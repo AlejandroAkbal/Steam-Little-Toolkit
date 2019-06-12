cb234271243bc152ed5057675e0501bab8f5296e
VoidlessSeven7 <37181533+VoidlessSeven7@users.noreply.github.com>
2019-06-12 20:03:47 +0200

    Added bypass filter
    And a little of cleaning

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 // ==UserScript==
-// @name         Steam owned games remover
-// @homepage     https://github.com/VoidlessSeven7/steam-in-library-remover/
-// @namespace    http://tampermonkey.net/
-// @version      1.10
-// @description  Removes from the store the games that you already own
-// @icon         http://store.steampowered.com/favicon.ico
-// @author       Alejandro Akbal
-// @include      https://store.steampowered.com/*
-// @include      http://store.steampowered.com/*
-// @grant        none
+
+// @name            Steam little toolkit
+// @namespace       http://tampermonkey.net/
+// @version         1.15
+// @author          Alejandro Akbal
+// @description     Little additions like removing owned games from the store and bypassing the external link filter
+
+// @source          https://github.com/VoidlessSeven7/steam-in-library-remover/
+// @website         https://akbal.dev
+// @icon            https://store.steampowered.com/favicon.ico
+
+// @updateURL       https://github.com/VoidlessSeven7/steam-in-library-remover/blob/master/script.js
+// @downloadURL     https://github.com/VoidlessSeven7/steam-in-library-remover/blob/master/script.js
+// @supportURL      https://github.com/VoidlessSeven7/steam-in-library-remover/issues
+
+// @match           http*://store.steampowered.com/*
+// @include         /^https?://(?:www\.)?steamcommunity\.com/linkfilter/.*$/
+
+// @run-at          document-end
+// @grant           none
+
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // User Settings
     const settings = {
+        bypassFilter: true,
         removeOwned: true,
         removeIgnored: false,
         removeOwnedRecentlyUpdatedList: false,
         removeIgnoredRecentlyUpdatedList: false
+    }
+
+    // Bypass the steam filter and go directly to the link
+    if (settings.bypassFilter === true) {
+
+        // This was implemented thanks to https://github.com/Jaex
+        const url = window.location.href,
+              regex = /^https?:\/\/(?:www\.)?steamcommunity\.com\/linkfilter\/.+?=(.+)$/i,
+              match = url.match(regex);
+
+        if (match) {
+            window.location.href = match[1];
+        }
+
     }
 
     // Functionality condensed into a single function
@@ -36,15 +62,15 @@
 
         /* ----- Category: Normal usage ----- */
 
-        if(urlContains('updated') < 0 && urlContains('bundle') < 0 ) {
+        if (urlContains('updated') < 0 && urlContains('bundle') < 0) {
 
             if (settings.removeOwned === true) {
-                removeElements( document.querySelectorAll(".ds_owned") );
+                removeElements(document.querySelectorAll(".ds_owned"));
                 console.log('Removed owned content');
             }
 
             if (settings.removeIgnored === true) {
-                removeElements( document.querySelectorAll(".ds_ignored") );
+                removeElements(document.querySelectorAll(".ds_ignored"));
                 console.log('Removed ignored content');
             }
 
@@ -52,25 +78,23 @@
 
 
         /* ----- Category: Recently updated ----- */
-
         else if (urlContains('updated') > -1) {
 
-                if (settings.removeOwnedRecentlyUpdatedList === true) {
-                    removeElements( document.querySelectorAll(".ds_owned") );
-                    console.log('Removed recently updated owned content');
-                }
+            if (settings.removeOwnedRecentlyUpdatedList === true) {
+                removeElements(document.querySelectorAll(".ds_owned"));
+                console.log('Removed recently updated owned content');
+            }
 
-                if (settings.removeIgnoredRecentlyUpdatedList === true) {
-                    removeElements( document.querySelectorAll(".ds_ignored") );
-                    console.log('Removed recently updated ignored content');
-                }
+            if (settings.removeIgnoredRecentlyUpdatedList === true) {
+                removeElements(document.querySelectorAll(".ds_ignored"));
+                console.log('Removed recently updated ignored content');
+            }
 
 
         } // End of Category: Recently updated
 
 
     }; // End of function start
-
 
     // Executing the script
     setTimeout(start, 1000);
@@ -85,14 +109,14 @@
 
 
 
-/* ----- Code that im not gonna use but could be good for optimizing in the future -----
+    /* ----- Code that im not gonna use but could be good for optimizing in the future -----
 
-    let isThereOwnedContent;
-    isThereOwnedContent = document.querySelectorAll(".ds_owned");
-    if (isThereOwnedContent.length > 0) {
-        alert("funciona");
-    }
+        let isThereOwnedContent;
+        isThereOwnedContent = document.querySelectorAll(".ds_owned");
+        if (isThereOwnedContent.length > 0) {
+            alert("funciona");
+        }
 
-*/
+    */
 
 })(); // End of Tampermonkey script
